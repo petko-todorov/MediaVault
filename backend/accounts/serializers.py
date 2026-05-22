75764a52c9a1015a70cf3ccab88dad4a43c554ad
Petko Todorov <p.todorov1015@gmail.com>
2026-05-22 14:55:18 +0300

    Add email and last name to UserMeSerializer.

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 
 User = get_user_model()
 
-# TODO: add more fields
+
 class UserMeSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'first_name']
+        fields = ['email', 'first_name', 'last_name']
