f2f63cbbae630915b996c041ff4f2fc30dc17381
Petko Todorov <p.todorov1015@gmail.com>
2026-06-23 11:16:35 +0300

    Initial commit

--- backend/games/views.py
+++ backend/games/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from games.models import UserGameLibrary
+from games.serializers import UserGameLibrarySerializer
+
+
+class CreateGameView(ListCreateAPIView):
+    serializer_class = UserGameLibrarySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return UserGameLibrary.objects.filter(user=self.request.user).select_related('game')
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        instance, created = UserGameLibrary.objects.get_or_create(
+            user=self.request.user,
+            game=serializer.validated_data['game'],
+        )
+
+        status_code = status.HTTP_201_CREATED if created else status.HTTP_200_OK
+        return Response(
+            self.get_serializer(instance).data,
+            status=status_code
+        )
