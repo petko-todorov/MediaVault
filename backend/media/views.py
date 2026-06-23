814d92d72e3bc5154497dc074361fa1c60e90c34
Petko Todorov <p.todorov1015@gmail.com>
2026-06-23 11:17:16 +0300

    Initial commit

--- backend/media/views.py
+++ backend/media/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from media.models import UserMediaLibrary
+from media.serializers import UserMediaLibrarySerializer
+
+
+class CreateMediaView(ListCreateAPIView):
+    serializer_class = UserMediaLibrarySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return UserMediaLibrary.objects.filter(user=self.request.user).select_related('media_item')
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        instance, created = UserMediaLibrary.objects.get_or_create(
+            user=self.request.user,
+            media_item=serializer.validated_data['media_item'],
+        )
+
+        status_code = status.HTTP_201_CREATED if created else status.HTTP_200_OK
+        return Response(
+            self.get_serializer(instance).data,
+            status=status_code
+        )
