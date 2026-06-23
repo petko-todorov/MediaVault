2f5f5daf8cd89a8e3bab170f5c0f79c05ddad56b
Petko Todorov <p.todorov1015@gmail.com>
2026-06-23 11:13:53 +0300

    Add UserGameLibrary serializer

--- backend/games/serializers.py
+++ backend/games/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-from games.models import Game
+
+from games.models import Game, UserGameLibrary
+
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = '__all__'
+
+
+class UserGameLibrarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserGameLibrary
+        fields = ['id', 'game', 'status', 'personal_rating', 'added_at']
+        read_only_fields = ['id', 'added_at']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['game'] = GameSerializer(instance.game).data
+        return representation
