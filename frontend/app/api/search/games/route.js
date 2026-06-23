3516abce6f73f1fc08a1b7911cfeb3c43b1ddef7
Petko Todorov <p.todorov1015@gmail.com>
2026-06-23 11:32:40 +0300

    Change game search route from POST to GET

--- frontend/app/api/search/games/route.js
+++ frontend/app/api/search/games/route.js
@@ -2,14 +2,15 @@ import axios from 'axios';
 import { requireAuth } from '@/lib/requireAuth';
 import { NextResponse } from 'next/server';
 
-export const POST = requireAuth(async (request, { accessToken }) => {
-    const body = await request.json();
+export const GET = requireAuth(async (request, { accessToken }) => {
+    const { searchParams } = new URL(request.url);
+    const q = searchParams.get('q');
     try {
         const res = await axios.get(
             `${process.env.NEXT_PUBLIC_BACKEND_URL}/search-games/`,
             {
                 headers: { Authorization: `Bearer ${accessToken}` },
-                params: { q: body.q },
+                params: { q },
             },
         );
         return NextResponse.json(res.data, { status: 200 });
