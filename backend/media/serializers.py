6d151798cdb1a9fc4023a12342a37203ee578a04
Petko Todorov <p.todorov1015@gmail.com>
2026-06-23 11:17:07 +0300

    Add UserMediaLibrary serializer and update MovieSeries serializer

--- backend/media/serializers.py
+++ backend/media/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-from media.models import MovieSeries
+
+from media.models import MovieSeries, UserMediaLibrary
+
 
 class MovieSeriesSerializer(serializers.ModelSerializer):
     class Meta:
         model = MovieSeries
         fields = '__all__'
+
+
+class UserMediaLibrarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserMediaLibrary
+        fields = ['id', 'media_item', 'status', 'personal_rating', 'added_at']
+        read_only_fields = ['id', 'added_at']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['media_item'] = MovieSeriesSerializer(instance.media_item).data
+        return representation
