f6c195d1074975c21fdd36375daa151725944d16
Petko Todorov <p.todorov1015@gmail.com>
2026-07-01 12:33:02 +0300

    Add game library route to urls

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from games.views import CreateGameView
+from games.views import CreateGameView, GameLibraryView
 
 urlpatterns = [
+    path('games/', GameLibraryView.as_view(), name='games-library'),
     path('add-game/', CreateGameView.as_view(), name='add-game'),
 ]
